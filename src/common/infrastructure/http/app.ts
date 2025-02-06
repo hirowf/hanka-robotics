15319d715f7328924e9e6f34e2c50766ec7b99c4
hirowf <hiroto.anderson@gmail.com>
2025-02-06 17:27:11 -0300

    API documentation

--- src/common/infrastructure/http/app.ts
+++ src/common/infrastructure/http/app.ts
@@ -1,12 +1,27 @@
 import cors from 'cors'
 import express from 'express'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 import { errorHandler } from './middlewares/errorHandler'
 import { routes } from './routes'
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+    },
+  },
+  apis: [],
+}
+const swaggerSpec = swaggerJSDoc(options)
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(routes)
 app.use(errorHandler)
 
--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -3,4 +3,5 @@ import { app } from './app'
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on ${env.PORT}`)
+  console.log(`API docs available at GET /docs`)
 })
