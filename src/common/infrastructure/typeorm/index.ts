01798b83678398214bdafcf3a910561090f9abdd
hirowf <hiroto.anderson@gmail.com>
2025-02-10 11:43:43 -0300

    fix: docker compose and enviroment varaible for database

---
+++ src/common/infrastructure/typeorm/index.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm'
+import { env } from '../env'
+
+export const dataSource = new DataSource({
+  type: env.DB_TYPE,
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  schema: env.DB_SCHEMA,
+  database: env.DB_NAME,
+  password: env.DB_PASS,
+  entities: ['**/entities/**/*.ts'],
+  migrations: ['**/migrations/**/*.ts'],
+  synchronize: false,
+  logging: false,
+})
